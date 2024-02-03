153043c6d775a71febfb4f6f3bc28fc4de73a2d5
Ppag <ppagdev@tuta.com>
2024-02-03 11:25:40 +0000

    added expression parsing and evaluating

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,19 +5,37 @@ extends Control
 func _ready():
 	print("WRONGCULATOR IS NOW ACTIVE!!!")
 
+func evaluateExpression():
+	var display = get_node("VSplitContainer/Label")
+	var expression = Expression.new() 
+	if expression.parse(display.text):
+		resetDisplay()
+		appendToDisplay("ERROR")
+		return
+	var result = str(expression.execute())
+	resetDisplay()
+	appendToDisplay(result)
+
 func resetDisplay():
 	var display = get_node("VSplitContainer/Label")
 	display.text = ""
 
 func appendToDisplay(arg):
 	var display = get_node("VSplitContainer/Label")
+	if display.text == "ERROR":
+		resetDisplay()
 	if display.text.length() >= 8:
+		display.text = display.text.substr(0,8)
 		return
 	display.text += arg
 
 func _on_button_pressed(arg):
-	appendToDisplay(str(arg))
+	appendToDisplay(arg)
 
 
 func _on_button_clr_pressed():
 	resetDisplay()
+
+
+func _on_button_equals_pressed():
+	evaluateExpression()
